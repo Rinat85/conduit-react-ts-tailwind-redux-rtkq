e85d55ac78a9d5e7441b433ea1e21dc5aad46691
Rinat85 <pacmanreal@gmail.com>
2022-11-04 17:24:59 +0500

    feat: add feed toggle

--- src/modules/feed/components/feed/feed.component.tsx
+++ src/modules/feed/components/feed/feed.component.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import { Container } from 'common/components/container/container.component';
 import { ArticleList } from 'modules/feed/components/article-list/article-list.component';
+import { FeedToggle } from 'modules/feed/components/feed-toggle/feed-toggle.component';
 // interface IFeedProps {};
 
 export const Feed: FC = () => {
   return (
     <Container>
+      <FeedToggle />
       <div className="flex">
         <ArticleList />
         <div className="w-1/4">tags</div>
